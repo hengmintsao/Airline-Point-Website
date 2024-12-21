95055fcab2ee7ff2230c8bf3472960d458b849c5
Hengmin Tsao <htsao2@myseneca.ca>
2024-12-21 15:10:18 -0500

    Create Layout.js MainNav.js, update index.js

---
+++ airline-point-website/components/Layout.js
@@ -0,0 +1,16 @@
+import MainNav from "./MainNav";
+import { Container } from "react-bootstrap";
+
+export default function Layout({children}){
+
+    return(
+    <>
+    <MainNav />
+    <br />
+    <Container>
+        {children}
+    </Container>
+    <br />
+    </>
+    );
+}
