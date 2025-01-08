4335240cc8f2ccb8ca469f9538ddefca33812f09
Hengmin Tsao <htsao2@myseneca.ca>
2025-01-08 18:58:00 -0500

    Add some design ideas about the costPerMile calcutor

--- airline-point-website/pages/costPerMile.js
+++ airline-point-website/pages/costPerMile.js
@@ -3,5 +3,14 @@ export default function costPerMiles(){
 
     return(
         <p>This is costPerMiles calculator. This calculator can help the user to understand how much they earn by daily expense, such as geocerires, restaurants...etc.</p>
+
+        //1.Please input the airline and miles required:
+
+        //2.Please insert your method of earning mileage: by flying, credit card program, buying points from their website....
+        //  Bootstrap range https://getbootstrap.com/docs/5.3/forms/range/
+        //  Input groups https://getbootstrap.com/docs/5.3/forms/input-group/
+
+
+        //3. submit and show the result (Hope there is a pie chart or something which and have visialization format to see the miles)
     )
 }
