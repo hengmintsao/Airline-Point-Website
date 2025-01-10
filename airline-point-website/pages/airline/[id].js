30860688e7bef39f92e5e9beb60cc093a1642a1a
Hengmin Tsao <htsao2@myseneca.ca>
2025-01-09 23:15:25 -0500

    Add "History" comment in "pages/airline/[id].js" to better track progress

--- airline-point-website/pages/airline/[id].js
+++ airline-point-website/pages/airline/[id].js
@@ -1,6 +1,16 @@
 import { useRouter } from "next/router";
 import AirlineCardDetail from "@/components/AirlineCardDetail";
 
+/* =============================================================History==============================================================================
+1. Date: 2025-Jan-07 Description: About.js can view card list, CardDetail basic feature complete. #TO-DO: Might need to consider storage folder and names might too similar to another folder
+
+
+
+=====================================================================================================================================================
+*/
+
+
+// This [id].js is for dynamically rendering pages based on the route parameter "id".
 export default function AirlineDetailPage() {
   const router = useRouter();
   const { id } = router.query;
