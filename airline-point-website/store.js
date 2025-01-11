436101f54c5fa72a759e5886c3b6d819406c2b63
Hengmin Tsao <htsao2@myseneca.ca>
2025-01-11 00:14:10 -0500

    Install jotai, create store.js(Haven't code anything yet)

---
+++ airline-point-website/store.js
@@ -0,0 +1,10 @@
+import { atom } from "jotai";
+
+/* =============================================================History==============================================================================
+1. Date: 2025-Jan-11 Description: store.js can allow users to keep track of previous searches and re-run/delete them if they want. #TO-DO: Haven't start yet
+
+
+=====================================================================================================================================================
+*/
+
+export const searchHistoryAtom = atom([]);
