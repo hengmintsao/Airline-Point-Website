21eae8c68407886819f5e1a1644f95c0f8c4f4b3
Hengmin Tsao <htsao2@myseneca.ca>
2025-01-14 15:38:34 -0500

    Create login.js page(haven't test yet) and empty register.js

---
+++ airline-point-website/pages/login.js
@@ -0,0 +1,71 @@
+import { Card, Button, Form, Alert} from "react-bootstrap";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { authenticateUser } from "@/lib/authenticate";
+import { getComparsion, getHistory } from "@/lib/userData";
+import { useAtom } from "jotai";
+import { comparsionAtom, searchHistoryAtom } from "@/store";
+
+
+/* =============================================================History==============================================================================
+1. Date: 2025-Jan-14 Description: Create Login page, input user and password  #TO-DO: Test
+
+
+
+=====================================================================================================================================================
+*/
+
+
+
+export default function Login(props){
+
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+    const [warning, setWarning] = useState("");
+    const [comparsionList, setComparsionList] = useAtom(comparsionAtom);
+    const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+    const router = useRouter();
+
+
+    // Fetch comparsion and history list and set into comparsionlist and history list
+    async function updateAtoms(){
+        setComparsionList(await getComparsion());
+        setSearchHistory(await getHistory());
+    }
+
+
+    // Function to handle after submit, authenticate user, fetch and push to comparsion page
+    async function handleSubmit(e){
+        e.preventDefault();
+        try{
+            await authenticateUser(user, password); // Authenticates user
+            await updateAtoms(); 
+            router.push('/comparsion');
+        }catch(err){
+            setWarning(err.message);
+        }
+    }
+
+
+
+    return (
+        <>
+        {warning && (<><br /><Alert variant="danger">{warning}</Alert></>)}
+        <Card bg="light">
+        <Card.Body><h2>Login</h2>Enter your login information below:</Card.Body>
+      </Card>
+      <br />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+        <Form.Label>User:</Form.Label><Form.Control type="text" id="userName" name="userName" onChange={e => setUser(e.target.value)}/>
+        </Form.Group>
+        <br />
+        <Form.Group>
+        <Form.Label>Password:</Form.Label><Form.Control type="password" id="password" name="password" onChange={e => setPassword(e.target.value)}/>
+        </Form.Group>
+        <br />
+        <Button variant="primary" className="pull-right" type="submit">Login</Button>
+      </Form>
+        </>
+    );
+}
