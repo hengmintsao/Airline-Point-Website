164718fcdb264fd2d1aec5c37478be95c0c30a64
Hengmin Tsao <htsao2@myseneca.ca>
2025-01-17 16:18:34 -0500

    Update MainNav.js(add logout function, add shown token part)

--- airline-point-website/components/MainNav.js
+++ airline-point-website/components/MainNav.js
@@ -2,6 +2,7 @@ import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { readToken, removeToken } from "@/lib/authenticate";
 
 
 export default function MainNav(){
@@ -9,7 +10,12 @@ export default function MainNav(){
     const [isExpanded, setIsExpanded] = useState(false);
     const router = useRouter();
 
+    let token = readToken();
 
+    function logout() {
+      removeToken();
+      router.push('/');
+    }
 
     return (
         <Navbar expand="lg" className="navbar bg-primary">
@@ -21,19 +27,23 @@ export default function MainNav(){
                 <Nav.Link href="/">Home</Nav.Link>
                 <Link href="/about" passHref legacyBehavior><Nav.Link>About</Nav.Link></Link>
                 <Nav.Link href="/calculator">Airport Calculator</Nav.Link>
-                <Nav.Link href="/costPerMile">Mile cost Calculator</Nav.Link>
-                {/* implement history */}
-                {/* <Link href="/history" passHref legacyBehavior>
-                  <Nav.Link>History</Nav.Link>
-                  </Link> */}
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                {token && <Link href="/costPerMile" passHref legacyBehavior> <Nav.Link href="/costPerMile">Mile cost Calculator</Nav.Link></Link>}
+              </Nav>
+              &nbsp;
+              <Nav>
+                {token ? (
+                  <NavDropdown title={token.userName || "User Name"} id="basic-nav-dropdown">
                 <Link href="/comparsion" passHref legacyBehavior><NavDropdown.Item onClick={() => setIsExpanded(false)}>Comparsion</NavDropdown.Item></Link>
                 <Link href="/history" passHref legacyBehavior><NavDropdown.Item active={router.pathname === '/history'} onClick={() => setIsExpanded(false)}>Search History</NavDropdown.Item></Link>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
+                  <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+                </NavDropdown>) : (
+                  <>
+                  <Link href="/register" passHref legacyBehavior><Nav.Link active={router.pathname === '/register'} onClick={() => setIsExpanded(false)}>Register</Nav.Link></Link>
+                  &nbsp;
+                  <Link href="/login" passHref legacyBehavior><Nav.Link active={router.pathname === '/login'} onClick={() => setIsExpanded(false)}>Login</Nav.Link></Link>
+                  </>
+                )}
               </Nav>
             </Navbar.Collapse>
           </Container>
