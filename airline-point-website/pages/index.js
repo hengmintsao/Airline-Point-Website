f97ba1369fe768872aeaaff1e9dd676ded578b5b
Hengmin Tsao <htsao2@myseneca.ca>
2025-01-22 12:19:27 -0500

    Update index.js(counter part)

--- airline-point-website/pages/index.js
+++ airline-point-website/pages/index.js
@@ -88,75 +88,98 @@ export default function Home() {
         </Container>
       </section>
       {/*About the features accomplish in this project */}
-      <section id="features" class="section-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
-                    <div class="section-title">
-                        <h1 class="display-4 fw-semibold">Features Accomplishment</h1>
-                        <div class="line"></div>
+      <section id="features" className="section-padding">
+        <div className="container">
+            <div className="row">
+                <div className="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+                    <div className="section-title">
+                        <h1 className="display-4 fw-semibold">Features Accomplishment</h1>
+                        <div className="line"></div>
                         <p>Easy to use those features!</p>
                     </div>
                 </div>
             </div>
-            <div class="row g-4 text-center">
-                <div class="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="150">
-                    <div class="service theme-shadow p-lg-5 p-4">
-                        <div class="iconbox">
-                            <i class="ri-pen-nib-fill"></i>
+            <div className="row g-4 text-center">
+                <div className="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="150">
+                    <div className="features theme-shadow p-lg-5 p-4">
+                        <div className="iconbox">
+                            <i className="ri-pen-nib-fill"></i>
                         </div>
-                        <h5 class="mt-4 mb-3">Airport Distance Calculator</h5>
+                        <h5 className="mt-4 mb-3">Airport Distance Calculator</h5>
                         <p>Use API to retrieve airport-related information (latitude and longitude), 
                           then use formula to calculate the distance in miles between two airports. </p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="250">
-                    <div class="service theme-shadow p-lg-5 p-4">
-                        <div class="iconbox">
-                            <i class="ri-stack-fill"></i>
+                <div className="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="250">
+                    <div className="features theme-shadow p-lg-5 p-4">
+                        <div className="iconbox">
+                            <i className="ri-stack-fill"></i>
                         </div>
-                        <h5 class="mt-4 mb-3">Pie Chart</h5>
+                        <h5 className="mt-4 mb-3">Pie Chart</h5>
                         <p>Use third-party npm package for quickly and easily creating pie chart in React</p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="350">
-                    <div class="service theme-shadow p-lg-5 p-4">
-                        <div class="iconbox">
-                            <i class="ri-ruler-2-fill"></i>
+                <div className="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="350">
+                    <div className="features theme-shadow p-lg-5 p-4">
+                        <div className="iconbox">
+                            <i className="ri-ruler-2-fill"></i>
                         </div>
-                        <h5 class="mt-4 mb-3">Web API with authentication</h5>
+                        <h5 className="mt-4 mb-3">Web API with authentication</h5>
                         <p>A Web API with authentication functionality is implemented to secure the API and ensure that only authorized users can access specific resources or perform operations. This is achieved using JSON Web Tokens (JWT) for stateless and secure authentication.</p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="450">
-                    <div class="service theme-shadow p-lg-5 p-4">
-                        <div class="iconbox">
-                            <i class="ri-pie-chart-2-fill"></i>
+                <div className="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="450">
+                    <div className="features theme-shadow p-lg-5 p-4">
+                        <div className="iconbox">
+                            <i className="ri-pie-chart-2-fill"></i>
                         </div>
-                        <h5 class="mt-4 mb-3">Comparison Table</h5>
+                        <h5 className="mt-4 mb-3">Comparison Table</h5>
                         <p>Using atomic state management with Jotai simplifies the implementation and management of the comparison table's state. It eliminates the need for boilerplate code, making the development process more efficient and maintainable.</p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="550">
-                    <div class="service theme-shadow p-lg-5 p-4">
-                        <div class="iconbox">
-                            <i class="ri-code-box-line"></i>
+                <div className="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="550">
+                    <div className="features theme-shadow p-lg-5 p-4">
+                        <div className="iconbox">
+                            <i className="ri-code-box-line"></i>
                         </div>
-                        <h5 class="mt-4 mb-3">Search History</h5>
+                        <h5 className="mt-4 mb-3">Search History</h5>
                         <p>After inputting data and generating the pie chart, the entered data is stored in an array using Jotai, allowing users to quickly access previously entered records.</p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="650">
-                    <div class="service theme-shadow p-lg-5 p-4">
-                        <div class="iconbox">
-                            <i class="ri-user-2-fill"></i>
+                <div className="col-lg-4 col-sm-6" data-aos="fade-down" data-aos-delay="650">
+                    <div className="features theme-shadow p-lg-5 p-4">
+                        <div className="iconbox">
+                            <i className="ri-user-2-fill"></i>
                         </div>
-                        <h5 class="mt-4 mb-3">AutoComplete</h5>
+                        <h5 className="mt-4 mb-3">AutoComplete</h5>
                         <p>After retrieving all data via the API, a designed AutoComplete dropdown dynamically displays matching options in real-time. Users can enter keywords or directly click on suggested information to quickly select the desired item, improving input efficiency and reducing errors.</p>
                     </div>
                 </div>
             </div>
         </div>
+    </section>
+    {/*Counter */}
+    <section id="counter" className="section-padding">
+      <div className="container text-center">
+        <div className="row g-4">
+          <div className="col-lg-3 col-sm-6">
+            <h1>9,000+</h1>
+            <h6>Airports</h6>
+          </div>
+          <div className="col-lg-3 col-sm-6">
+            <h1>4 Types</h1>
+            <h6>Earn points methods</h6>
+          </div>
+          <div className="col-lg-3 col-sm-6">
+            <h1>3 Alliances included</h1>
+            <h6>Analysis pros & cons</h6>
+          </div>
+          <div className="col-lg-3 col-sm-6">
+            <h1>Canada based</h1>
+            <h6>From the perspective of departing from Canada, looking at various airlines</h6>
+          </div>
+        </div>
+      </div>
     </section>
     </>
   );
