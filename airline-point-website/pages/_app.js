dbf6e2c07ac858861d1f0a499323c6690b75a360
Hengmin Tsao <htsao2@myseneca.ca>
2025-02-02 10:22:50 -0500

    Update _app.js(SWRConfig)

--- airline-point-website/pages/_app.js
+++ airline-point-website/pages/_app.js
@@ -1,16 +1,28 @@
 import AOS from "aos"; // Anime on scorll library installation
 import "aos/dist/aos.css"; // Anime on scorll library installation
-import '@/styles/globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "@/components/Layout";
-import RouteGuard from '@/components/RouteGuard';
-import { useEffect } from 'react'; 
-
-
-
-
+import RouteGuard from "@/components/RouteGuard";
+import { useEffect } from "react";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
+  const fetcher = async (url) => {
+    const res = await fetch(url);
+
+    // If the status code is not in the range 200-299,
+    // still try to parse and throw it.
+    if (!res.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+
+      // Attach extra info to the error object.
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+    return res.json();
+  };
 
   // Use Anime on scroll library
   useEffect(() => {
@@ -18,15 +30,17 @@ export default function App({ Component, pageProps }) {
       easing: "ease-out-cubic",
       once: true,
       offset: 50,
-      duration: 1000
+      duration: 1000,
     });
   }, []);
 
   return (
     <RouteGuard>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      </RouteGuard>
+      <SWRConfig value={{ fetcher }}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SWRConfig>
+    </RouteGuard>
   );
 }
